61a9f06bd24293eaede1ab182f47c3291918fcd5
Angell Belger <angellbelger@hotmail.com>
2022-07-30 14:20:01 -0300

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+var txtSubject = window.document.querySelector('input#txtSubject')
+
+
+
+function subjects(){
+    numberSubject = Number(txtSubject)
+
+    for (var c = 0; c < numberSubject; c++){
+
+    }
+}
