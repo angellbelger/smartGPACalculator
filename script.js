3f9a0e07d787ad0646cdabf930d4404663dd9757
Angell Belger <82967046+angellbelger@users.noreply.github.com>
2022-10-22 12:33:05 -0300

    update

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-var txtSubject = window.document.querySelector('input#txtSubject')
+let subject = window.document.querySelector('input#subject')
+let note = window.document.querySelector('input#note')
+let credit = window.document.querySelector('input#credit')
+let tabSub = window.document.querySelector('select#tabSub')
+
+let subjectsPro = {subject: '', note: 0, credit: 0}
 
 
 
-function subjects(){
-    numberSubject = Number(txtSubject)
-
-    for (var c = 0; c < numberSubject; c++){
-
+function addSub (paramSub= 'Nonename', paramNote= 0, paramCred= 0){
+    if (subject.value.length == 0){
+        window.alert('Please, type a value for subject.')
+    }else{
+        subjectsPro.subject = subject.value
     }
+    if (note.value < 0 || note.value > 10){
+        window.alert('Please, type a value between 0 and 10.')
+    }else{
+        subjectsPro.note = Number(note.value)
+    }
+    if (credit.value.length == 0){
+        window.alert('Type a valie credit.')
+    }else{
+        subjectsPro.credit = Number(credit.value)
+    }
+    let item = document.createAttribute('option')
+    item.text = `${subjectsPro.subject.value} - Note: ${subjectsPro.note.value} - Credit: ${subjectsPro.credit}`
+    tabSub.appendChild(item)
+
 }
