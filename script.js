59778ba9e0f193d44bef3fc9f201204f69e3f4fd
Angell Belger <angellbelger@hotmail.com>
2023-12-23 17:06:25 -0300

    update

--- script.js
+++ script.js
@@ -1,34 +1,108 @@
-let subject = window.document.querySelector('input#subject')
-let note = window.document.querySelector('input#note')
-let credit = window.document.querySelector('input#credit')
-let tabSub = window.document.querySelector('select#tabSub')
+// Vetor para armazenar as disciplinas
+var subjectsArray = [];
 
-let subjectsPro = {subject: '', note: 0, credit: 0}
+// Função para adicionar uma disciplina
+function addSub() {
+    // Obter os valores dos campos
+    var subject = document.getElementById("subject").value.trim();
+    var note = parseFloat(document.getElementById("note").value);
+    var credit = parseFloat(document.getElementById("credit").value);
 
+    // Verificar se os valores são válidos
+    if (subject && !isNaN(note) && !isNaN(credit)) {
+        // Adicionar a disciplina ao vetor
+        subjectsArray.push({
+            subject: subject,
+            note: note,
+            credit: credit
+        });
 
+        // Atualizar a tabela com as disciplinas
+        updateSubjectsTable();
 
-function addSub (paramSub= 'Nonename', paramNote= 0, paramCred= 0){
-    if (subject.value.length == 0){
-        window.alert('Please, type a value for subject.')
-    }else{
-        subjectsPro.subject = subject.value
+        // Limpar os campos
+        document.getElementById("subject").value = "";
+        document.getElementById("note").value = "";
+        document.getElementById("credit").value = "";
+    } else {
+        console.error("Por favor, preencha todos os campos corretamente.");
     }
-    if (note.value < 0 || note.value > 10){
-        window.alert('Please, type a value between 0 and 10.')
-    }else{
-        subjectsPro.note = Number(note.value)
-    }
-    if (credit.value.length == 0){
-        window.alert('Type a valie credit.')
-    }else{
-        subjectsPro.credit = Number(credit.value)
-    }
-    let list = []
-    list.push(subjectsPro)
-    
-    let item = document.createElement('option')
-    item.text = `The value is ${list.subjectsPro.subject}`
-    item.value = list[0].subject.value
-    tabSub.appendChild(item)
+}
 
+// Função para remover uma disciplina
+function removeSub(index) {
+    subjectsArray.splice(index, 1);
+    updateSubjectsTable(); // Atualiza a tabela após a remoção
+}
+
+// Função para atualizar o select com as disciplinas
+function updateSubjectsSelect() {
+    var tabSub = document.getElementById("tabSub");
+    // Limpar as opções existentes
+    tabSub.innerHTML = "";
+
+    // Adicionar as disciplinas como opções
+    for (var i = 0; i < subjectsArray.length; i++) {
+        var option = document.createElement("option");
+        option.text = subjectsArray[i].subject + ": " + subjectsArray[i].note + " / " + subjectsArray[i].credit;
+        tabSub.add(option);
+    }
+}
+
+// Função para calcular o GPA e atualizar a tabela
+function calculateGPA() {
+    var totalCredits = 0;
+    var totalWeightedPoints = 0;
+
+    // Iterar sobre as disciplinas
+    for (var i = 0; i < subjectsArray.length; i++) {
+        // Converter a nota para a escala de 0 a 4
+        var scaledGrade = (subjectsArray[i].note * 4) / 10;
+
+        totalWeightedPoints += scaledGrade * subjectsArray[i].credit;
+        totalCredits += subjectsArray[i].credit;
+    }
+
+    // Calcular o GPA na escala de 0 a 4
+    var gpa = totalCredits > 0 ? totalWeightedPoints / totalCredits : 0;
+
+    // Exibir o GPA no rodapé da tabela
+    document.getElementById("gpa").textContent = gpa.toFixed(2);
+}
+
+// Função para atualizar a tabela com as disciplinas e calcular o GPA
+function updateSubjectsTable() {
+    var tableBody = document.getElementById("tableBody");
+
+    // Limpar as linhas existentes
+    tableBody.innerHTML = "";
+
+    // Adicionar as disciplinas como linhas na tabela
+    for (var i = 0; i < subjectsArray.length; i++) {
+        var row = tableBody.insertRow();
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3); // Nova célula para o botão de remoção
+
+        cell1.innerHTML = subjectsArray[i].subject;
+        cell2.innerHTML = subjectsArray[i].note;
+        cell3.innerHTML = subjectsArray[i].credit;
+
+        // Adicionar botão de remoção com o sinal "-"
+        var removeButton = document.createElement("button");
+        removeButton.innerHTML = "  —  ";
+        removeButton.onclick = createRemoveHandler(i); // Usar uma função de fábrica para criar handlers únicos
+        cell4.appendChild(removeButton);
+    }
+
+    // Calcular o GPA e atualizar o rodapé
+    calculateGPA();
+}
+
+// Função para criar um handler de remoção único para cada botão
+function createRemoveHandler(index) {
+    return function () {
+        removeSub(index);
+    };
 }
